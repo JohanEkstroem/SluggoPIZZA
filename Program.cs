0b9aa0537ddac20ac81fe1f3bde7be5161b77849
JohanEkstroem <90635692+JohanEkstroem@users.noreply.github.com>
2023-09-20 08:23:42 +0200

    Add Swagger documentation

--- Program.cs
+++ Program.cs
@@ -1,5 +1,20 @@
+using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Swagger configuration
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+  c.SwaggerDoc("v1", new OpenApiInfo { Title = "SluggoPIZZA API", Description = "Keep track of your pizzas", Version = "v1" });
+});
 var app = builder.Build();
+
+// Add Swagger
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+  c.SwaggerEndpoint("/swagger/v1/swagger.json", "SluggoPIZZA API V1");
+});
 
 app.MapGet("/", () => "Hello World!");
 
