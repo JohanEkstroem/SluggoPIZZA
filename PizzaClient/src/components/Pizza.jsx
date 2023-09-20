1fcd41708eebb30b1ebc385e00b54dd880e20823
JohanEkstroem <90635692+JohanEkstroem@users.noreply.github.com>
2023-09-20 15:27:38 +0200

    Fetch and render data from API

--- PizzaClient/src/components/Pizza.jsx
+++ PizzaClient/src/components/Pizza.jsx
@@ -1,24 +1,39 @@
 import { useState, useEffect } from 'react';
+import PizzaList from './PizzaList';
 
 export default function Pizza() {
   const [data, setData] = useState([]);
-  const [maxId, setMaxId] = useState(0);
-
+  const [error, setError] = useState(null);
+  const API_URL = 'http://localhost:5000/api/pizzas';
+  const headers = {
+    'Content-Type': 'application/json',
+  };
   useEffect(() => {
     // TODO: fetch data from API
+    fetchPizzaData();
   }, []);
 
+  const fetchPizzaData = () => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => setError(error));
+  };
   const handleCreate = (item) => {
     // TODO: create item on API
-  }
+  };
 
   const handleUpdate = (item) => {
     // TODO: update item on API
-  }
+  };
 
   const handleDelete = (id) => {
     // TODO: delete item on API
-  }
+  };
 
-  return <div>Pizza</div>;
+  return (
+    <div>
+      <PizzaList data={data} />
+    </div>
+  );
 }
--- PizzaClient/src/components/PizzaList.jsx
+++ PizzaClient/src/components/PizzaList.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export default function PizzaList() {
+export default function PizzaList({ data }) {
   const [editingId, setEditingID] = useState(null);
   const [formData, setFormData] = useState({
     id: '',
@@ -10,18 +10,34 @@ export default function PizzaList() {
 
   const handleFormChange = (event) => {
     // TODO: handle setFormdata
-  }
+  };
 
   const handleSubmit = (event) => {
     // TODO: handle submiting data
-  }
+  };
 
   const handleEdit = (item) => {
     // TODO: handle editing data
-  }
+  };
 
   const handleCancelEdit = () => {
     // TODO: empty editID
-  }
-  return <div>PizzaList</div>;
+  };
+  return (
+    <div>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>
+            <div>
+              {item.name} - {item.description}
+            </div>
+            <div>
+              <button>Edit</button>
+              <button>Delete</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
